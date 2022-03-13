12a456aa4d854c995b6d1b59aeaabc9e49bc612a
marycynthia <innocentmarycynthia3@gmail.com>
2022-03-13 10:57:36 -0700

    commit 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Alwasy 0 (success)
+ *
+ */
+int main(void)
+{
+	char alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		putchar (alphabet);
+		alphabet++;
+	}
+		putchar ('\n');
+		return (0);
+}
+
+
