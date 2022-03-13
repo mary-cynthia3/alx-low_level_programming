b325abb6736675e7a213871084a3470c107abf90
marycynthia <innocentmarycynthia3@gmail.com>
2022-03-13 11:14:24 -0700

    commit 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase
+ *
+ * Return: Alwasy 0 (success)
+ */
+int main(void)
+{
+	char alpha_lower = 'a', alpha_upper = 'A';
+
+	while (alpha_lower <= 'z')
+	{
+		putchar (alpha_lower);
+		alpha_lower++;
+	}
+	while (alpha_upper <= 'Z')
+	{
+		putchar (alpha_upper);
+		alpha_upper++;
+	}
+		putchar ('\n');
+		return (0);
+}
+
+
