b1548f6ee8590c171fc5c7a9feb18a0d64880ef8
marycynthia <innocentmarycynthia3@gmail.com>
2022-03-14 19:05:32 -0700

    commit 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints 0 -9
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+	
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
