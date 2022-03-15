c0f84b676e8a7013ee6f35f2d748e5647787a2e0
marycynthia <innocentmarycynthia3@gmail.com>
2022-03-14 19:18:46 -0700

    commit 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints 0 -9
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
