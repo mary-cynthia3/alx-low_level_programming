b4a17060517e37aa75abc6448e4d945d84a8b7cb
marycynthia <innocentmarycynthia3@gmail.com>
2022-03-14 20:55:20 -0700

    commit 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,12 +8,13 @@
 int main(void)
 {
 	char alphabet;
+
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 	{
 		if (alphabet == 'q' || alphabet == 'e')
-			{
-				continue;
-			}
+		{
+			continue;
+		}
 		putchar (alphabet);
 	}
 	putchar ('\n');
