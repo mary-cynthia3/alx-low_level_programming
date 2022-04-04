622d32e301778c7bb2dd302f17af690645bb45b0
marycynthia <innocentmarycynthia3@gmail.com>
2022-04-03 21:04:12 -0700

    commit 3

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,15 +17,15 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+	for (j = 0; argv[i][j] != '\0'; j++)
+	{
+		if (!isdigit(argv[i][j]))
 		{
-			if (!isdigit(argv[i][j]))
-			{
-					printf("Error\n");
-					return (1);
-			}
+			printf("Error\n");
+			return (1);
 		}
-		add += atoi(argv[i]);
+	}
+	add += atoi(argv[i]);
 	}
 	printf("%d\n", add);
 	return (0);
