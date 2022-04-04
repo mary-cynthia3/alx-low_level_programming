77b675452c3359945a3f710c198b78111954583a
marycynthia <innocentmarycynthia3@gmail.com>
2022-04-03 19:41:22 -0700

    commit 10

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-	while (n < 98)
+		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
@@ -22,7 +22,7 @@ void print_to_98(int n)
 	}
 	else if (n > 98)
 	{
-	while (n > 98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
