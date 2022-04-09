9f8b1199ca576ef48572efcc56770b2e7a4136ec
marycynthia <innocentmarycynthia3@gmail.com>
2022-04-08 19:25:47 -0700

    commit 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,10 +19,10 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	if (new_size == 0 && ptr != NULL)
-		{
-			free(ptr);
-			return (NULL);
-		}
+	{
+		free(ptr);
+		return (NULL);
+	}
 	if (ptr == NULL)
 		ptr = malloc(new_size);
 	if (new_size == old_size)
